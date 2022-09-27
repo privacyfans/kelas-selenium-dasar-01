8d867aaf10671b595a1177d2a976a7690e157fd8
Irfan Luthfi Irawan <privacyfans@gmail.com>
2022-09-27 21:23:04 +0700

    Before and After Annotation Fix

--- src/test/java/com/herokuapp/internet/LoginTest.java
+++ src/test/java/com/herokuapp/internet/LoginTest.java
@@ -79,81 +79,81 @@ public class LoginTest {
         //driver.quit();
     }
 
-//    @Parameters({"username_u","password_u","errorMessage_u"})
-//    @Test(priority = 2, groups = {"negativeTest","smokeTest"},enabled = false)
-//    public void usernameFailed(String username,String password, String message){
-//        // create driver
-////        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
-////        WebDriver driver = new ChromeDriver();
+    @Parameters({"username_u","password_u","errorMessage_u"})
+    @Test(priority = 2, groups = {"negativeTest","smokeTest"},enabled = false)
+    public void usernameFailed(String username,String password, String message){
+        // create driver
+//        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+
+//        System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver.exe");
+//        WebDriver driver = new FirefoxDriver();
 //
-////        System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver.exe");
-////        WebDriver driver = new FirefoxDriver();
-////
-////        // open page
-////        String url ="http://the-internet.herokuapp.com/login";
-////        driver.get(url);
-//        //driver.manage().window().maximize();
-//        // input username
-//        WebElement usernameField = driver.findElement(By.id("username"));
-//        usernameField.sendKeys(username);
-//        // input password
-//        WebElement passwordField = driver.findElement(By.id("password"));
-//        passwordField.sendKeys(password);
-//        // click button login
-//        WebElement buttonLogin = driver.findElement(By.xpath("//button[@type='submit']"));
-//        buttonLogin.click();
-//        // validate loagin success
-//        // 1. url change to /secure
-//        String expedtedURL = "http://the-internet.herokuapp.com/login";
-//        String actualURL = driver.getCurrentUrl();
-//        Assert.assertEquals(actualURL,expedtedURL,"URL expedted dan URL actual Tidak Sama");
+//        // open page
+//        String url ="http://the-internet.herokuapp.com/login";
+//        driver.get(url);
+        //driver.manage().window().maximize();
+        // input username
+        WebElement usernameField = driver.findElement(By.id("username"));
+        usernameField.sendKeys(username);
+        // input password
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys(password);
+        // click button login
+        WebElement buttonLogin = driver.findElement(By.xpath("//button[@type='submit']"));
+        buttonLogin.click();
+        // validate loagin success
+        // 1. url change to /secure
+        String expedtedURL = "http://the-internet.herokuapp.com/login";
+        String actualURL = driver.getCurrentUrl();
+        Assert.assertEquals(actualURL,expedtedURL,"URL expedted dan URL actual Tidak Sama");
+
+        // 2. message error username
+        String expectedMessage = "Your username is invalid!";
+        WebElement toastMessage = driver.findElement(By.id("flash"));
+        String actualMessage = toastMessage.getText();
+        Assert.assertTrue(actualMessage.contains(message),"Actual Message Username Tidak sama dengan Expected Message");
+
+
+        sleep(3000);
+       // driver.quit();
+    }
+
+    @Parameters({"username_p","password_p","errorMessage_p"})
+    @Test(priority = 3,groups = {"negativeTest"},enabled = false)
+    public void passwordFailed(String username,String password, String message){
+        // create driver
+//        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
 //
-//        // 2. message error username
-//        String expectedMessage = "Your username is invalid!";
-//        WebElement toastMessage = driver.findElement(By.id("flash"));
-//        String actualMessage = toastMessage.getText();
-//        Assert.assertTrue(actualMessage.contains(message),"Actual Message Username Tidak sama dengan Expected Message");
-//
-//
-//        sleep(3000);
-//       // driver.quit();
-//    }
-//
-//    @Parameters({"username_p","password_p","errorMessage_p"})
-//    @Test(priority = 3,groups = {"negativeTest"},enabled = false)
-//    public void passwordFailed(String username,String password, String message){
-//        // create driver
-////        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
-////        WebDriver driver = new ChromeDriver();
-////
-////        // open page
-////        String url ="http://the-internet.herokuapp.com/login";
-////        driver.get(url);
-//        //driver.manage().window().maximize();
-//        // input username
-//        WebElement usernameField = driver.findElement(By.id("username"));
-//        usernameField.sendKeys(username);
-//        // input password
-//        WebElement passwordField = driver.findElement(By.id("password"));
-//        passwordField.sendKeys(password);
-//        // click button login
-//        WebElement buttonLogin = driver.findElement(By.xpath("//button[@type='submit']"));
-//        buttonLogin.click();
-//        // validate login failed
-//        // 1. url change to /login
-//        String expedtedURL = "http://the-internet.herokuapp.com/login";
-//        String actualURL = driver.getCurrentUrl();
-//        Assert.assertEquals(actualURL,expedtedURL,"URL expedted dan URL actual Tidak Sama");
-//
-//        // 2. message error username
-//        String expectedMessage = "Your password is invalid!";
-//        WebElement toastMessage = driver.findElement(By.id("flash"));
-//        String actualMessage = toastMessage.getText();
-//        Assert.assertTrue(actualMessage.contains(message),"Actual Message Password Tidak sama dengan Expected Message");
-//
-//        sleep(3000);
-//       // driver.quit();
-//    }
+//        // open page
+//        String url ="http://the-internet.herokuapp.com/login";
+//        driver.get(url);
+        //driver.manage().window().maximize();
+        // input username
+        WebElement usernameField = driver.findElement(By.id("username"));
+        usernameField.sendKeys(username);
+        // input password
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys(password);
+        // click button login
+        WebElement buttonLogin = driver.findElement(By.xpath("//button[@type='submit']"));
+        buttonLogin.click();
+        // validate login failed
+        // 1. url change to /login
+        String expedtedURL = "http://the-internet.herokuapp.com/login";
+        String actualURL = driver.getCurrentUrl();
+        Assert.assertEquals(actualURL,expedtedURL,"URL expedted dan URL actual Tidak Sama");
+
+        // 2. message error username
+        String expectedMessage = "Your password is invalid!";
+        WebElement toastMessage = driver.findElement(By.id("flash"));
+        String actualMessage = toastMessage.getText();
+        Assert.assertTrue(actualMessage.contains(message),"Actual Message Password Tidak sama dengan Expected Message");
+
+        sleep(3000);
+       // driver.quit();
+    }
 
     @Parameters({"username","password","errorMessage","browser"})
     @Test(priority = 4,groups = {"negativeTest"})
