e2386cd8abff11383fa2560bd4683a1ea16c5e68
Irfan Luthfi Irawan <privacyfans@gmail.com>
2022-09-27 18:04:27 +0700

    Fixing Nama File

--- src/test/java/com/herokuapp/internet/NegativeLoginIT.java
+++ src/test/java/com/herokuapp/internet/NegativeLoginIT.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class NegativeLogin {
+public class NegativeLoginIT {
     @Test
     public void usernameFailed(){
         // create driver
--- src/test/java/com/herokuapp/internet/PositiveLoginIT.java
+++ src/test/java/com/herokuapp/internet/PositiveLoginIT.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PositiveLogin {
+public class PositiveLoginIT {
     @Test
     public void loginSuccess(){
         // create driver
